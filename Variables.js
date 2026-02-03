fc598cfde6fae58adda2844d8baf86bbb0c16b66
nitesh-cpu <niteshkumar860326@gmail.com>
2026-02-04 00:07:26 +0530

    Initial commit: Add JavaScript files

---
+++ Variables.js
@@ -0,0 +1,18 @@
+const accountId=2345;
+let accountEmail="nitesh@gamil.com";
+var accountPassword="Nitesh@123";
+accountCity="Hazaribagh";
+
+
+// accountId=2; // not allowed
+
+console.log(accountId)
+
+accountEmail="asdfgh@gamil.com";
+accountPassword="23456789"
+accountCity="Ranchi";
+
+console.table([accountId,accountPassword,accountCity]);
+
+/* prefer not to use var beacuse of issue in block scope and functional scope
+*/
