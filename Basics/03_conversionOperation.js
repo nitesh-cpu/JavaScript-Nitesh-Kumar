64e7c8c4a6463249f610894c55f09fb3b4de284f
nitesh-cpu <niteshkumar860326@gmail.com>
2026-02-05 01:36:47 +0530

    conversion and operation

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+
+// if score is --> "33" => 33
+// "23sdf" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+// 1 => true; 0=>false
+// "" => false
+// "hitesh" =>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+// ******************** Operations ****************
+
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "nitesh"
+let str3 = str1+str2
+console.log(str3)
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1"+(2+2)) // 14
+// console.log("1"+2+2) // 122
+// console.log(2+2+"1") // 41
+
+// console.log((4 + 3) * 5 % 3)
+
+
+// console.log(+true);  // 1
+// console.log(+"");    // 0
+
+let num1,num2,num3
+
+num1=num2=num3=2+3
+
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
