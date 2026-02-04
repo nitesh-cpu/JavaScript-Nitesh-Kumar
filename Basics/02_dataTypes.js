4fde5787e1fc0716758ff567654c18fe5bd90102
nitesh-cpu <niteshkumar860326@gmail.com>
2026-02-05 00:30:09 +0530

    datatypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // trreat all Js code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3
+  +
+  4) // code readability should be high
+
+console.log("Hello Nitesh")
+
+//ecmaScript is standard for java
+// one more docmentaion is there tc39.
+
+
+let name="Nitesh kumar"
+let age=18;
+let isLogged=false
+let state
+
+
+// DATATYPES -->
+// number =>2 to power 53
+// bigint 
+// string => " ", ''
+// boolean => true/false
+// null =>standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined ); // undefined
+console.log(typeof null) //object
+
